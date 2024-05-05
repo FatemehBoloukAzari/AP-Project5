2da2296c24c950b2505f652b698994f46b6743d2
Fatemeh <fsstamx@gmail.com>
2024-05-05 22:59:33 +0330

    add files

---
+++ include/menu.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "global_stuff.h"
+
+enum SpriteType
+{
+    WALNUT,
+    PEASHOOTER,
+    SNOWPEA,
+    MELONPULT,
+    SUNFLOWER
+};
+
+class MenuItem
+{
+public:
+    void render();
+    void update();
+private:
+    //int x;
+    //int y;
+    Texture item_texture;
+    int remaining_cooldown_time;
+    SpriteType sprite_type;
+};
+
+class Menu
+{
+public:
+    void update();
+    void render();
+private:
+    vector <MenuItem*> menu_items;
+};
