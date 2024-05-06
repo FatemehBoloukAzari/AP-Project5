1be856a1907843e9e51f7da289cf120448cf271f
Fatemeh <fsstamx@gmail.com>
2024-05-06 18:17:22 +0330

    added sun

---
+++ include/sun.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "global_stuff.h"
+
+class Sun
+{
+public:
+    Sun(bool, int x_pos = 0, int y_pos = 0);
+    // void update();
+    void render(RenderWindow &window);
+private:
+    int x;
+    int y;
+    bool is_moving;
+    Texture sun_texture;
+    Sprite sun_sprite;
+    void move();
+};
---
+++ src/sun.cpp
@@ -0,0 +1,34 @@
+#include "sun.h"
+
+const int SUN_WIDTH = 100;
+const int SUN_HEIGHT = 100;
+
+Sun::Sun(bool moving, int x_pos, int y_pos)
+{
+    if (moving)
+    {
+        x_pos = rand() % (WIDTH - SUN_WIDTH);
+        y_pos = 0;
+    }
+    x = x_pos;
+    y = y_pos;
+    is_moving = moving;
+    sun_texture.loadFromFile(PICS_PATH + "sun.png");
+    sun_sprite.setTexture(sun_texture);
+}
+
+void Sun::render(RenderWindow &window)
+{
+    double scale_x = (double)SUN_WIDTH / sun_texture.getSize().x;
+    double scale_y = (double)SUN_HEIGHT / sun_texture.getSize().y;
+    sun_sprite.setScale(scale_x, scale_y);
+    sun_sprite.setPosition(x, y);
+    window.draw(sun_sprite);
+}
+
+const int SUN_SPEED = 5;
+
+void Sun::move()
+{
+    y += SUN_SPEED;
+}
