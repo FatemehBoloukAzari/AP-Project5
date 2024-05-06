fc2ceb02281bdef345059ff8ba57813a7628da02
imSaDy <sadegh.samadi2483@gmail.com>
2024-05-06 19:38:07 +0330

    add zombie and game_object

--- include/game_object.h
+++ include/game_object.h
@@ -1,11 +1,17 @@
 #pragma once 
 
+#include "global_stuff.h"
+
 class GameObject {
 public :
+	GameObject (int ,int ,SpriteType) ; 
 	virtual void update() = 0 ; 
-	virtual void render() = 0 ; 
+	virtual void render(RenderWindow &window) = 0 ;
 protected :
-	int health ; 
+	int health ;
 	int x ,y ;
 	int time_since_last_damage ;
-}; 
+	Texture item_texture;
+    Sprite item_sprite;
+    SpriteType sprite_type;
+};
---
+++ src/game_object.cpp
@@ -0,0 +1,33 @@
+#include "game_object.h"
+
+GameObject::GameObject(int _x ,int _y ,SpriteType _sprite_type){
+    x = _x ; 
+    y = _y ; 
+    sprite_type = _sprite_type ;
+    switch (sprite_type){
+        case WALNUT:
+            item_texture.loadFromFile(PICS_PATH + "defai.png");
+            break;
+        case PEASHOOTER:
+            item_texture.loadFromFile(PICS_PATH + "nokhode_shooti.png");
+            break;
+        case SNOWPEA:
+            item_texture.loadFromFile(PICS_PATH + "nokhode_barfi.png");
+            break;
+        case MELONPULT:
+            item_texture.loadFromFile(PICS_PATH + "hendevane_part_kon.png");
+            break;
+        case SUNFLOWER:
+            item_texture.loadFromFile(PICS_PATH + "aftab_gardoon.png");           
+            break;
+        case REGULAR: 
+            item_texture.loadFromFile(PICS_PATH + "zombie_adi.png"); 
+            break ; 
+        case HAIRMETALGARGANTUAR:
+            item_texture.loadFromFile(PICS_PATH + "giant.png");
+            break ; 
+        default:
+            break;
+    }
+    item_sprite.setTexture(item_texture);
+}
