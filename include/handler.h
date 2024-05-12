8b2163d2cd47c808cc60f25a80eaae70302cb5e5
Fatemeh <fsstamx@gmail.com>
2024-05-12 08:38:28 +0330

    Change victory screen

--- include/handler.h
+++ include/handler.h
@@ -23,7 +23,9 @@ public:
     SpriteType get_random_zombie_type();
     void main_menu_render(RenderWindow &window);
     void game_over_render(RenderWindow &window);
+    void victory_render(RenderWindow &window);
     void in_game_initialization();
+    void game_over_initialization();
     void handle_zombie_damages(double ,double); 
     void clean_dead_plants(); 
     void check_moving_stopped_zombies(double ,double); 
@@ -41,10 +43,12 @@ private:
     vector <Bullet*> bullets_in_line[5]; 
     Menu menu;
     Clock sun_generating_clock;
+    Clock game_over_clock;
     int number_of_suns;
     int sun_interval;
     void render_sun_bank(RenderWindow &window);
     void handle_adding_plant(Event event, SpriteType adding_sprite, double, double);
     void render_cursor_following_sprite(RenderWindow &window);
     void add_plant(SpriteType sprite_type, double pos_x, double pos_y ,int row ,int col);
+    int get_number_of_zombies();
 };
